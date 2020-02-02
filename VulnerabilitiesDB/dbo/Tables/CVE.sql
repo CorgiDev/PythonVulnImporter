31f983eeecdcaab2898d58d21fe70500a24c8901
Elizabeth Gray <9934135+CorgiDev@users.noreply.github.com>
2020-02-02 03:17:03 -0500

    Add project files.

---
+++ VulnerabilitiesDB/dbo/Tables/CVE.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[CVE] (
+    [Name]        NVARCHAR (50)  NOT NULL,
+    [Status]      NVARCHAR (50)  NOT NULL,
+    [Description] NVARCHAR (MAX) NOT NULL,
+    [References]  NVARCHAR (MAX) NULL,
+    [Phase]       NVARCHAR (50)  NULL,
+    [Votes]       NVARCHAR (250) NULL,
+    [Comments]    NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_CVE] PRIMARY KEY CLUSTERED ([Name] ASC)
+);
+
